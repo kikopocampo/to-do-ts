bccff810d93809001f76533fe066933226aab5cb
Francisco <kikopocampo@gmail.com>
2023-02-02 14:50:01 -0700

    clean app and create initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import TodoList from "./components/TodoList";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos((prev) => [...prev, { id: Math.random().toString(), text: text }]);
+    console.log(text);
+  };
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
