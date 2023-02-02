8df7209aa9e178ed56f1a536b150d01e60870c1e
Francisco <kikopocampo@gmail.com>
2023-02-02 15:09:25 -0700

    add styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
     setTodos((prev) => [...prev, { id: Math.random().toString(), text: text }]);
-    console.log(text);
+  };
+  const todoDeleteHandler = (id: string) => {
+    setTodos((prev) => {
+      return todos.filter((item) => item.id !== id);
+    });
   };
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
